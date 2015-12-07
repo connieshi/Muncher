0ea8906f29ba3f008f461665273978268f19fae7
Connie Shi <connieshi@172-16-201-88.DYNAPOOL.NYU.EDU>
2015-12-07 15:55:15 -0500

    add project

---
+++ Muncher/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  Muncher
+//
+//  Created by Connie Shi on 12/7/15.
+//  Copyright © 2015 Connie Shi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Muncher/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  Muncher
+//
+//  Created by Connie Shi on 12/7/15.
+//  Copyright © 2015 Connie Shi. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "DraggableViewBackground.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    DraggableViewBackground *draggableBackground = [[DraggableViewBackground alloc] initWithFrame:self.view.frame];
+    [self.view addSubview: draggableBackground];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
